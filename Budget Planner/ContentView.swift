4a7a4993b533fb3394fe6f112a1c82784a00004a
Ashton Price <apric9@mybce.catholic.edu.au>
2021-05-28 14:35:40 +1000

    Initial Commit

---
+++ Budget Planner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Budget Planner
+//
+//  Created by Ashton Price on 28/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
